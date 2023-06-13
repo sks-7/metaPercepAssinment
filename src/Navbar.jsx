b30290b479b5633f94b2411a1ccfc04cf57f9dac
sks-7 <sachinsinghofficial7@gmail.com>
2023-06-13 17:33:33 +0530

    added

---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>Title</h3>
+
+      <div className="contant">
+        <p>Home</p>
+        <p>About</p>
+        <p>Blog</p>
+        <p>Contact</p>
+        <button>Button</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
